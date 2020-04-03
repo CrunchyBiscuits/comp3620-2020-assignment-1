06f1012a785a300663d220c837a9a62c597c9a99
Zheyuan Zhang <u6870923@anu.edu.au>
2020-04-04 07:40:58 +0800

    update ids Zheyuan Zhang 2020.4.4

--- ids_search.py
+++ ids_search.py
@@ -33,33 +33,27 @@ def dls(problem: SearchProblem, bound):
     s0 = problem.get_initial_state()
     frontier = Stack()
     frontier.push((s0, 0))
+    answer = {s0: ""}
     visited = [s0]
-    return recursive_dls(problem, frontier, visited, bound)
+    return recursive_dls(problem, frontier, visited, bound, answer)
 
 
-def recursive_dls(problem: SearchProblem, frontier, visited, bound):
+def recursive_dls(problem: SearchProblem, frontier, visited, bound, answer):
     cutoff_occurred = False
     if frontier.is_empty():
         return True
     state, depth = frontier.pop()
     if problem.goal_test(state):
-        answer = []
-        node = visited[-1]
-        for path in reversed(visited):
-            for successor, action, cost in problem.get_successors(path):
-                if successor == node:
-                    node = path
-                    answer.append(action)
-        answer.reverse()
-        return answer
+        return answer[state].split()
     elif depth == bound:
         return True
     else:
         for successor, action, cost in problem.get_successors(state):
             if successor not in visited:
                 frontier.push((successor, depth+1))
+                answer[successor] = answer[state] + " " + str(action)
                 visited.append(successor)
-            result = recursive_dls(problem, frontier, visited, bound)
+            result = recursive_dls(problem, frontier, visited, bound, answer)
             if result and type(result) == bool:
                 cutoff_occurred = True
             else:
