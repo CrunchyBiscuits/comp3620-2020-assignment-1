e453581a35235bbb5994f687ae4779063fea1aac
Zheyuan Zhang <u6870923@anu.edu.au>
2020-04-09 22:49:13 +0800

    finish MST Zheyuan Zhang 2020.4.9

--- heuristics.py
+++ heuristics.py
@@ -23,6 +23,7 @@ Position = Tuple[int, int]
 YellowBirds = Tuple[Position]
 State = Tuple[Position, YellowBirds]
 
+
 # -------------------------------------------------------------------------------
 # A set of heuristics which are used with a PositionSearchProblem
 # You do not need to modify any of these.
@@ -52,6 +53,7 @@ null = null_heuristic
 manhattan = manhattan_heuristic
 euclidean = euclidean_heuristic
 
+
 # -------------------------------------------------------------------------------
 # You have to implement the following heuristics for Q4 of the assignment.
 # It is used with a MultiplePositionSearchProblem
@@ -79,12 +81,46 @@ def every_bird_heuristic(state: State,
     heuristic_value = 0
 
     """ *** YOUR CODE HERE *** """
-    min_distance = 100
-    for yellow_bird in yellow_birds:
-        if problem.maze_distance(position, yellow_bird) < min_distance:
-            min_distance = problem.maze_distance(position, yellow_bird)
-            heuristic_value = min_distance
-    return heuristic_value
+    # # MST
+    # node_count = len(yellow_birds) + 1
+    #
+    # # set the nodes in MST
+    # u_ds = [position]
+    # for yellow_bird in yellow_birds:
+    #     u_ds.append(yellow_bird)
+    # v_ds = u_ds
+    #
+    # # create adjacency matrix
+    # edges = {}
+    # for u in u_ds:
+    #     for v in v_ds:
+    #         if u == v:
+    #             continue
+    #         else:
+    #             edges[(u, v)] = problem.maze_distance(u, v)
+    #
+    # # create mst
+    # current_node = u_ds[0]
+    # visited = [current_node]
+    # min_distance = 1000
+    # for _ in range(node_count - 1):
+    #     temp_node = None
+    #     for a, b in edges.keys():
+    #         if a == current_node and b not in visited:
+    #             if edges[(a, b)] < min_distance:
+    #                 min_distance = edges[(a, b)]
+    #                 temp_node = b
+    #     current_node = temp_node
+    #     visited.append(current_node)
+    #     heuristic_value += min_distance
+    #
+    # return heuristic_value
 
+    if yellow_birds:
+        heuristic_value += problem.maze_distance(position, yellow_birds[0])
+        for i in range(len(yellow_birds) - 1):
+            heuristic_value += problem.maze_distance(yellow_birds[i], yellow_birds[i+1])
+
+    return heuristic_value
 
 every_bird = every_bird_heuristic
